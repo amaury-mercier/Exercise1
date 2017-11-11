ba3dcf9bc0ea9bca3731b1905856200e492f70a6
Amaury Mercier <amaury.mercier@polytechnique.edu>
2017-11-11 18:19:08 +0100

    Hello user

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
+import java.util.*;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.print("Please enter your name : ");
+		Scanner sc = new Scanner(System.in);
+		String userName = sc.next();
+		HelloUser user = new HelloUser(userName);
+		user.greetUser();
+		sc.close();		
 	}
 
 }
