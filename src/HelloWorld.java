a9fb3cf68863300996b02fbb283bd4d52d7361f3
Amaury Mercier <amaury.mercier@polytechnique.edu>
2017-11-11 18:30:07 +0100

    yet

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello ASE2017 - How are you now ? :)");
+		System.out.println("Hello ASE2017 - How are you yet ? :)");
 		System.out.print("Please enter your name : ");
 		Scanner sc = new Scanner(System.in);
 		String userName = sc.next();
